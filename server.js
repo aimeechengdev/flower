7b997bb14f97c389a9cd54bf948cacf08c17a97d
aimeechengdev <aimeechengdev@gmail.com>
2015-05-31 18:06:35 +0000

    added testget and testpost

--- server.js
+++ server.js
@@ -181,7 +181,14 @@ app.get('/confirm',function(req,res){
   train();
   res.end("confirmed");
 });
-
+app.get('/testget',function(req,res){
+  console.log("testget called");
+  res.end("testget confirmed");
+});
+app.post('/testpost',function(req,res){
+  console.log("testpost called");
+  res.end("testpost confirmed");
+});
 server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
   var addr = server.address();
   console.log("Flower server listening at", addr.address + ":" + addr.port);
